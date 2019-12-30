5bfbe9bf8ca82c4800205ec91780d1c3e5fda769
Mohammed Tarafdar <MohammedTarafdar>
2019-12-30 09:51:13 -0500

    if else ladder added

---
+++ src/main/java/control/statement/IfElseLadder.java
@@ -0,0 +1,33 @@
+package control.statement;
+
+public class IfElseLadder {
+
+
+    public static void main(String[] args) {
+
+        int numberOfDay = 5;
+
+        if (numberOfDay == 1) {
+            System.out.println("Monday");
+        }
+        else if(numberOfDay == 2) {
+            System.out.println("Tuesday");
+        }
+        else if(numberOfDay == 3) {
+            System.out.println("Wednesday");
+        }
+        else if(numberOfDay == 4) {
+            System.out.println("Thursday");
+        }
+        else if(numberOfDay == 5) {
+            System.out.println("Friday");
+        }
+        else if(numberOfDay == 6) {
+            System.out.println("Saturday");
+        }
+        else if(numberOfDay == 7) {
+            System.out.println("Sunday");
+        }
+
+    }
+}
