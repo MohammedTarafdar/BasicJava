ae68757ee98fde184c68d6b02d1ef35cfb3f1de8
Mohammed Tarafdar <MohammedTarafdar>
2019-12-29 21:35:18 -0500

    if and if else statement added

---
+++ src/main/java/control/statement/IfElseExample.java
@@ -0,0 +1,17 @@
+package control.statement;
+
+public class IfElseExample {
+    // This program demonstate the use of if else statement
+
+    public static void main(String[] args) {
+        int mark = 39;
+
+        if (mark >= 40){
+            System.out.println("Student get pass mark");
+        }
+        else{
+            System.out.println("Student get fail mark");
+        }
+    }
+
+}
---
+++ src/main/java/control/statement/IfExample.java
@@ -0,0 +1,12 @@
+package control.statement;
+
+public class IfExample {
+    // Java program to demonstate the use of if statement
+
+    public static void main(String[] args) {
+        int marks =39;
+        if (marks >= 40){
+            System.out.println("Student gets pass marks");
+        }
+    }
+}
