4da59e1db35745ff4b5b28b10344757fe84dcd6e
MohammedTarafdar <mtarafdar96@gmail.com>
2020-04-12 19:56:35 -0400

    Local Instance and Static variable added

---
+++ src/main/java/basic/FirstJavaClass.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class FirstJavaClass {
+
+    public static void main(String [] arg){
+        System.out.println("Java is a high level, robust, object oriented and secure programming language.");
+
+    }
+
+}
